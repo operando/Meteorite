09a7c66ad811434939108c9837896c1f7f8492a9
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-11-23 22:22:46 +0900

    Change sample code

--- meteorite-sample/src/main/java/com/os/operando/meteorite/sample/MyApplication.java
+++ meteorite-sample/src/main/java/com/os/operando/meteorite/sample/MyApplication.java
@@ -2,16 +2,13 @@ package com.os.operando.meteorite.sample;
 
 import android.app.Application;
 
-import com.os.operando.meteor.MeteorActivityLifecycleCallbacks;
-import com.os.operando.meteor.MeteorNotification;
-import com.os.operando.meteorite.MeteoriteActivity;
+import com.os.operando.meteorite.Meteorite;
 
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        MeteorNotification.show(this, MeteoriteActivity.createComponentName(this));
-        registerActivityLifecycleCallbacks(new MeteorActivityLifecycleCallbacks());
+        Meteorite.init(this);
     }
 }
